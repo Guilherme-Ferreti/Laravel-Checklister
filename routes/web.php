2791d8c1d44ea6410d91bbbb5e79fd5633fe6bfc
Guilherme-Ferreti <guiferretis@gmail.com>
2021-06-11 14:10:56 -0300

    Welcome page and top-right menu links

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
 use \App\Http\Controllers\Admin;
+use Illuminate\Support\Facades\Route;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,9 +20,10 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
-
 Route::group(['middleware' => 'auth'], function () {
+    Route::get('/welcome', [\App\Http\Controllers\PageController::class, 'welcome'])->name('welcome');
+    Route::get('/consultation', [\App\Http\Controllers\PageController::class, 'consultation'])->name('consultation');
+    
     Route::group(['prefix' => 'admin', 'as' => 'admin.', 'middleware' => 'is_admin'], function () {
         Route::resource('pages', Admin\PageController::class)->only(['edit', 'update']);
         Route::resource('checklist_groups', Admin\ChecklistGroupController::class);

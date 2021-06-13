703daa4985093db96052be9cff74b4c2bc467c03
Guilherme-Ferreti <guiferretis@gmail.com>
2021-06-13 17:36:39 -0300

    Fix Tasks Sortable Livewire Component

--- resources/views/livewire/tasks-table.blade.php
+++ resources/views/livewire/tasks-table.blade.php
@@ -1,8 +1,17 @@
 <div>
-    <table class="table table-responsive-sm" wire:sortable="updateTaskOrder">
+    <table class="table table-responsive-sm">
         <tbody>
             @foreach ($tasks as $task)
-                <tr wire:sortable.item="{{ $task->id }}" wire:key="task-{{ $task->id }}">
+                <tr>
+                    <td>
+                        @if ($task->position > 1)
+                            <a wire:click.prevent="task_up({{ $task->id }})" href="#">&uarr;</a>
+                        @endif
+                        
+                        @if ($task->position < $tasks->max('position'))
+                            <a wire:click.prevent="task_down({{ $task->id }})" href="#">&darr;</a>
+                        @endif
+                    </td>
                     <td>{{ $task->name }}</td>
                     <td>
                         <a href="{{ route('admin.checklists.tasks.edit', [$checklist, $task]) }}" class="btn btn-sm btn-primary">{{ __('Edit') }}</a>
